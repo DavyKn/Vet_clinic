5ceb7a63237c1809f3d71e3ee5b02db36c321526
DavyKK <davykk04@gmail.com>
2022-09-24 14:03:17 +0300

    data, table added and modified and queried

--- queries.sql
+++ queries.sql
@@ -90,6 +90,30 @@ SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
 
+-- What animals belong to Melody Pond?
+SELECT name FROM animals INNER JOIN owners ON owner_id = owners.id WHERE full_name = 'Melody Pond';
+
+-- List of all animals that are pokemon (their type is Pokemon).
+SELECT a.name FROM animals AS a INNER JOIN species AS s ON species_id = s.id WHERE s.name = 'Pokemon';
+
+-- List all owners and their animals, remember to include those that don't own any animal.
+SELECT a.name, o.full_name FROM animals AS a RIGHT JOIN owners As o ON owner_id = o.id;
+
+-- How many animals are there per species?
+SELECT s.name, COUNT(a.name) FROM animals AS a INNER JOIN species AS s ON species_id = s.id GROUP BY s.name;
+
+-- List all Digimon owned by Jennifer Orwell.
+SELECT a.name FROM animals AS a INNER JOIN owners AS o ON owner_id = o.id INNER JOIN species AS s ON species_id = s.id WHERE s.name = 'Digimon' AND o.full_name = 'Jennifer Orwell';
+
+-- List all animals owned by Dean Winchester that haven't tried to escape.
+SELECT o.full_name AS owner,a.name AS  Not_Tried_To_ESCAPE FROM animals AS a INNER JOIN owners As o ON owner_id = o.id 
+WHERE full_name = 'Dean Winchester' and escape_attempts = 0 ;
+
+-- Who owns the most animals?
+SELECT o.full_name AS owner,COUNT(*) as owns   FROM animals AS a INNER JOIN owners As o ON owner_id = o.id  
+GROUP BY o.full_name ORDER BY owns DESC LIMIT 1  ;
+
+
 
 
 
--- schema.sql
+++ schema.sql
@@ -16,3 +16,38 @@ CREATE TABLE animals (
 
 --add a column species_id to the animals tables
 ALTER TABLE animals ADD COLUMN species VARCHAR(30);
+
+-- Create a table named owners 
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+-- Create a table named species
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Modify animals table:
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals DROP COLUMN id;
+
+ALTER TABLE animals ADD COLUMN id INT GENERATED ALWAYS AS IDENTITY, ADD PRIMARY KEY (id);
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
+
+
+
+
